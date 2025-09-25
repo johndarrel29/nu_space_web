3208fe704bf89bdec27131b6579056b61103a452
Kevin Abgao <kabgao@gmail.com>
2025-09-26 00:07:38 +0800

    version 3.7.0
    added babel to hide console log on production build

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -5,7 +5,14 @@ import globals from "globals";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended", 'plugin:react/recommended'], languageOptions: { globals: globals.browser }
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended", 'plugin:react/recommended'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": ["error", { allow: ["warn", "error"] }]
+    }
   },
   pluginReact.configs.flat.recommended,
 ]);
